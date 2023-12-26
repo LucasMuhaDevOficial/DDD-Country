5906316d2fae9aa4caf699010764040ca92fef08
LUCAS MARTINEZ <101533422+LUCASMARTINEZPROGRAMADOR2022@users.noreply.github.com>
2023-12-26 19:26:11 -0300

    Atualizacao do CSS: refatoração e aprimoramento de estilos e lógica

---
+++ index.js
@@ -0,0 +1,27 @@
+// Suponha que a taxa de câmbio de dólar para euro seja 0.85
+const dollarToEuroExchangeRate = 0.85;
+
+function calculateCost() {
+  const countrySelect = document.getElementById('country');
+  const areaCodeInput = document.getElementById('areaCode');
+  const durationInput = document.getElementById('duration');
+  const resultDiv = document.getElementById('result');
+
+  const country = countrySelect.options[countrySelect.selectedIndex].text;
+  const areaCode = areaCodeInput.value;
+  const duration = parseFloat(durationInput.value);
+
+  // Adicione lógica para obter dados de DDD, códigos de área e outros fatores, se necessário
+
+  // Suponha que o custo base por minuto em dólar seja 0.05
+  const baseCostPerMinute = 0.05;
+
+  // Lógica de cálculo do custo em dólares
+  const costInDollars = baseCostPerMinute * duration;
+
+  // Converta o custo de dólares para euros
+  const costInEuros = costInDollars * dollarToEuroExchangeRate;
+
+  // Exiba o resultado
+  resultDiv.innerHTML = `País: ${country}<br>Custo em Dólares: $${costInDollars.toFixed(2)}<br>Custo em Euros: €${costInEuros.toFixed(2)}`;
+}
